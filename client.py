04e52f886fa616ac7abb0f8d563e6d214e60288c
Dane O <danonator821@gmail.com>
2026-07-03 19:17:18 -0400

    better ip

--- client.py
+++ client.py
@@ -1,20 +1,22 @@
 import socket
 import threading
 
+
 def print_msg(msg):
     print(f" >> {msg}")
 
+
 alias = ""
-while alias != 'y' and alias != 'n':
+while alias != "y" and alias != "n":
     alias = input("add alias y/n? ").lower()
 
-if alias == 'y':
+if alias == "y":
     alias = input("add alias: ")
     print(f"alis: '{alias}'")
 
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 4872))
+client.connect((socket.gethostbyname(socket.gethostname()), 4872))
 # client.connect((alias, 4872))
 
 while True:
@@ -23,3 +25,4 @@ while True:
     data = client.recv(1024)
     # print(f"\t>> {repr(data)}")
     threading.Thread(target=print_msg, args=repr(data))
+
